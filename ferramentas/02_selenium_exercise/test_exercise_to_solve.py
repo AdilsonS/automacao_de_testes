19c66aeeb938697b581108acef3298da1e609ef1
Adilson Souza <33134619+AdilsonS@users.noreply.github.com>
2024-10-28 21:44:54 -0300

    feat: Atualizando testes.

--- ferramentas/02_selenium_exercise/test_exercise_to_solve.py
+++ ferramentas/02_selenium_exercise/test_exercise_to_solve.py
@@ -1,32 +1,65 @@
-import random
 import pathlib
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from time import sleep
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver import ChromeOptions
 
+#Função criada para obter webdriver do Chrome, no meu ambiente estava tendo erro ao obter como indicado na aula, por isso a necessidade dessas tratativas.
+def start_driver():
+    try:
+        driver = webdriver.Chrome()
+        options = ChromeOptions()
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+    except:
+        chrome_driver_path = "/home/adilson/.cache/selenium/chromedriver/linux64/130.0.6723.69/chromedriver"
+        chrome_binary_path = "/usr/bin/google-chrome"
+        
+        chrome_options = Options()
+        chrome_options.binary_location = chrome_binary_path
+        
+        service = Service(chrome_driver_path)
+        driver = webdriver.Chrome(service=service, options=chrome_options)
+    finally:
+        return driver
 
-def test_sample_page():
-    file_path = pathlib.Path(__file__).parent.resolve()
-    driver = webdriver.Chrome()
-    driver.get(f"file:////{file_path}/sample-exercise_.html")
-    generate_code(driver)
-    sleep(5)
-    code = driver.find_element(By.ID, "my-value")
-    input = driver.find_element(By.ID, "input")
-    input.clear()
-    input.send_keys(code.text)
-    test_bnt = driver.find_element(By.NAME, "button")
-    test_bnt.click()
-
-    alert = driver.switch_to.alert
-    alert.accept()
-
-    result = driver.find_element(By.ID, "result")
-    assert result.text == f"It workls! {code.text}!"
-
-    driver.quit()
-
-
-def generate_code(driver):
-    generate = driver.find_element(By.NAME, "generate")
-    generate.click()
+def test():
+    try:
+        driver = start_driver()        
+        file_path = pathlib.Path(__file__).parent / "sample-exercise.html"
+        driver.get(f"file://{file_path}")
+        
+        title = driver.title
+        assert title == "Sample page"
+        time.sleep(2)
+        
+        text_box = driver.find_element(by=By.ID, value="input")
+        generate_button = driver.find_element(by=By.NAME, value="generate")
+        submit_button = driver.find_element(by=By.NAME, value="button")
+        
+        generate_button.click()        
+        generated_value = WebDriverWait(driver, 5).until(
+            EC.visibility_of_element_located( (By.ID, "my-value") )
+        )
+        generated_text = generated_value.text
+        
+        text_box.clear()
+        text_box.send_keys(generated_text)
+        submit_button.click()
+        time.sleep(2)
+        
+        alert = WebDriverWait(driver, 5).until(
+            EC.alert_is_present()
+        )
+        alert.accept()
+        
+        message = driver.find_element(by=By.ID, value="result")
+        value = message.text
+        assert value == f"It workls! {generated_text}!"
+        time.sleep(2)        
+    finally:
+        driver.quit()
